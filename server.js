40548937336d154abb4ab2b6617187d67627f0a7
philippschulte <philipp.schulte@ymail.com>
2015-08-07 16:04:10 -0600

    Completed step 2

--- server.js
+++ server.js
@@ -1,21 +1,70 @@
 'use strict';
 
+//#####################################################
+//Personal Information:
+//#####################################################
+var name = {name: 'Philipp Schulte'};
+var location = {location: 'Provo, UT'};
+var hobbies = ['Programming', 'Fussball', 'Ping Pong'];
+var occupations = ['Student', 'Bridge Engineer', 'Student'];
+var mentions = ['https://www.linkedin.com/pub/philipp-schulte/b5/9b4/180'];
+var references = ['Polina Marchenko', 'Reinhard Schulz', 'Michael Fischer'];
+
+//#####################################################
 //Dependencies:
+//#####################################################
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
+//#####################################################
 //Express:
+//#####################################################
 var app = express();
 
+//#####################################################
 //Port:
+//#####################################################
 var port = process.argv[2] || 3000;
 
+//#####################################################
 //Middelware:
+//#####################################################
 app.use('/', bodyParser.json());
 app.use('/', cors());
 
+//#####################################################
+//Routes:
+//#####################################################
+
+//GET /api/name:
+app.get('/api/name', function(req, res) {
+	res.json(name);
+});
+
+//GET /api/location:
+app.get('/api/location', function(req, res) {
+	res.json(location);
+});
+
+//GET /api/hobbies:
+app.get('/api/hobbies', function(req, res) {
+	res.json({hobbies: hobbies});
+});
+
+//GET /api/occupations:
+app.get('/api/occupations', function(req, res) {
+	res.json({occupations: occupations});
+});
+
+//GET /api/occupations/latest:
+app.get('/api/occupations/latest', function(req, res) {
+	res.json({latestOccupation: occupations[occupations.length - 1]});  
+});
+
+//#####################################################
 //Connection to server:
+//#####################################################
 app.listen(port, function(err) {
 	if(err) {
 		console.log(err);
